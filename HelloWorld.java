8e6cbd33621f3b303185d09db3bff5b3c4a57a12
AjmalRiyaz <mhdajmalriyaz73@gmail.com>
2018-07-26 15:20:17 +0530

    Adding Hello World File and last name to initial file

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Ajmalriyaz");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myChallenge = 1000  - myTotal;
+
+        System.out.println("The difference between 1000 & myTotal is: " + myChallenge);
+    }
+}
